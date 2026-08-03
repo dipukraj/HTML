d82c94697067888583ef38a5a267fa38a9765933
Dipu K Raj <kumardipu1436@gmail.com>
2026-08-03 19:35:29 +0530

    update

--- dipu.css/tut38.js
+++ dipu.css/tut38.js
@@ -1,10 +1,13 @@
 console.log("This is Tutorial 38");
 
+function greet(name){
+    console.log(name + " is a good boy");
+}
 let name = "Dipu";
 let name1 = "Bittu";
 let name2 = "Rahul";
 let name3 = "Mukesh";
-console.log(name + " is a good boy");
-console.log(name1 + " is a good boy");
-console.log(name2 + " is a good boy");
-console.log(name3 + " is a good boy");
+// console.log(name + " is a good boy");
+// console.log(name1 + " is a good boy");
+// console.log(name2 + " is a good boy");
+// console.log(name3 + " is a good boy");
